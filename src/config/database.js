87343e3135dc66f18f1dd3f2a055ccf113b795e8
Firiz <unitytyan@gmail.com>
2021-10-06 16:08:40 +0900

    f#14 envのhostとportを指定できるように変更及びconfig.jsonでもportを指定できるよう変更
    - Sequalizeを初期化時にhostとportを指定できるように変更しconfig.jsonでもportを指定できるようにしました
    - database.jsにauthenticateを追加

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,18 @@
 require('dotenv/config');
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
-module.exports=new Sequelize(
-  process.env.POSTGRES_DB, 
-  process.env.POSTGRES_USER, 
-  process.env.POSTGRES_PASS, 
-  {dialect: 'postgres' }
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASS,
+  {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    dialect: 'postgres'
+  }
 );
+(async () => {
+  await sequelize.authenticate();
+  console.log("authenticated postgres");
+})();
+module.exports = sequelize;
