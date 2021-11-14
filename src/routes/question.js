ddcfb9176df4025610413aab68d7b61cc1d2d9ff
hiro2029 <hiro.2029@icloud.com>
2021-11-14 17:59:03 +0900

    微修正を実施

--- src/routes/question.js
+++ src/routes/question.js
@@ -6,6 +6,7 @@ router.get('/', question.getQuestionList)
     .get('/:id', question.getQuestion)
     .get('/:id/info', question.getQuestionInfo)
     .post('/', question.createQuestion)
+    .post('/api/questions/:id/blank', question.createBlankSelectQuestion)
     .delete('/:id', question.deleteQuestion)
     .put('/:id', question.updateQuestion)
 
