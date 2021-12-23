0ca2e948c708ac32a0bb95d205c9ab361db73826
hiro2029 <hiro.2029@icloud.com>
2021-12-23 21:16:23 +0900

    微修正を実施

--- src/seeders/20211004055534-user.js
+++ src/seeders/20211004055534-user.js
@@ -6,21 +6,21 @@ module.exports = {
       {
         name: 'TEST1',
         mail: 'test1@example.com',
-        password_hash: 'password',
+        password_hash: '$2a$10$NA45d9Hb1EBGvYxUMqhpHuFuV5BCm0sukdPIiCq5Dw6h6nNfGBYFG',
         role: '学習者',
         created_at: new Date()
       },
       {
         name: 'TEST2',
         mail: 'test2@example.com',
-        password_hash: 'password',
+        password_hash: '$2a$10$NA45d9Hb1EBGvYxUMqhpHuFuV5BCm0sukdPIiCq5Dw6h6nNfGBYFG',
         role: '学習者',
         created_at: new Date()
       },
       {
         name: 'TEST3',
         mail: 'test3@example.com',
-        password_hash: 'password',
+        password_hash: '$2a$10$NA45d9Hb1EBGvYxUMqhpHuFuV5BCm0sukdPIiCq5Dw6h6nNfGBYFG',
         role: '教授者',
         created_at: new Date()
       }
