2b0345c4569382628329b50f5fde6e93e9064ff8
hiro2029 <hiro.2029@icloud.com>
2022-08-21 18:07:22 +0900

    認証機能の修正

--- src/middleware/authJwt.js
+++ src/middleware/authJwt.js
@@ -4,14 +4,14 @@ const sequelize = require("../config/database");
 const models = initModels(sequelize);
 
 const User = models.users;
+const Role =models.roles;
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.js");
 // const db = require("../models");
 // const User = db.user;
 
 exports.verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
-
+  const token = req.headers["x-access-token"];
   if (!token) {
     return res.status(403).send({
       message: "No token provided!"
@@ -24,47 +24,78 @@ exports.verifyToken = (req, res, next) => {
         message: "Unauthorized!"
       });
     }
-    req.userId = decoded.id;
+    req.userId = decoded.user_id;
+    req.token = token;
+    
     next();
   });
 };
 
 exports.isAdmin = (req, res, next) => {
   User.findByPk(req.userId).then(user => {
-    user.getRoles().then(roles => {
-      for (let i = 0; i < roles.length; i++) {
-        if (roles[i].name === "教授者") {
-          next();
-          return;
-        }
-      }
-
-      res.status(403).send({
-        message: "Require Admin Role!"
-      });
+    if(user.role==='教授者'){
+      req.user_id = user.user_id;
+      req.name = user.name;
+      req.mail = user.mail,
+      req.role = user.role,
+      req.token = req.token
+      next();
       return;
-    });
-  });
-};
-
-exports.isModerator = (req, res, next) => {
-  User.findByPk(req.userId).then(user => {
-    user.getRoles().then(roles => {
-      for (let i = 0; i < roles.length; i++) {
-        if (roles[i].name === "学習者") {
-          next();
-          return;
-        }
-      }
-
+    }else{
       res.status(403).send({
-        message: "Require Moderator Role!"
+        message: "Require User Role!"
       });
-    });
+    }
   });
 };
 
-exports.isModeratorOrAdmin = (req, res, next) => {
+exports.isUser = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    // console.log(user.role);
+    if(user.role==='学習者'){
+      req.user_id = user.user_id;
+      req.name = user.name;
+      req.mail = user.mail,
+      req.role = user.role,
+      req.token = req.token
+      next();
+      return;
+    }else{
+      res.status(403).send({
+        message: "Require User Role!"
+      });
+    }
+    // Role.findOne({
+    //   where: { role: req.body.role }
+    // })
+    // .then(role => {
+    //   console.log(role);
+    //   if(role.role='学習者'){
+    //     next();
+    //     return;
+    //   }
+    // })
+    // .catch(error => {
+    //   res.status(403).send({
+    //     message: "Require User Role!"
+    //   })
+    // })
+    // user.getRoles().then(roles => {
+    //   for (let i = 0; i < roles.length; i++) {
+    //     if (roles[i].name === "学習者") {
+    //       next();
+    //       return;
+    //     }
+    //   }
+
+    //   res.status(403).send({
+    //     message: "Require User Role!"
+    //   });
+    // });
+  });
+};
+
+exports.isUserOrAdmin = (req, res, next) => {
   User.findByPk(req.userId).then(user => {
     user.getRoles().then(roles => {
       for (let i = 0; i < roles.length; i++) {
@@ -80,7 +111,7 @@ exports.isModeratorOrAdmin = (req, res, next) => {
       }
 
       res.status(403).send({
-        message: "Require Moderator or Admin Role!"
+        message: "Require User or Admin Role!"
       });
     });
   });
