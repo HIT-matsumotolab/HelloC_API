a0e62b04870eca84388c79c0a31f6428c59bb5e8
hiro2029 <hiro.2029@icloud.com>
2022-10-19 01:33:55 +0900

    いらないかも

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,10 +4,11 @@ const router = Router();
 const { verifySignUp, authJwt } = require("../middleware");
 
 router.use(function(req, res, next) {
-    res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-    );
+    // res.header(
+        // "Access-Control-Allow-Headers",
+        // "Access-Control-Allow-Origin", "*",
+        // "x-access-token, Origin, Content-Type, Accept"
+    // );
     next();
 })
 .post(
