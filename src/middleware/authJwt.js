5a5552133f10502ac85881f4b894777cd9b1e73d
hiro2029 <hiro.2029@icloud.com>
2022-10-23 18:02:27 +0900

    エラーメッセージ変更

--- src/middleware/authJwt.js
+++ src/middleware/authJwt.js
@@ -21,7 +21,7 @@ exports.verifyToken = (req, res, next) => {
   jwt.verify(token, config.secret, (err, decoded) => {
     if (err) {
       return res.status(401).send({
-        message: "Unauthorized!"
+        message: err.message
       });
     }
     req.userId = decoded.user_id;
